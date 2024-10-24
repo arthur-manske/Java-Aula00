f5d2cb80236938aed099deb01835be70af43791b
Arthur de S. Manske <arthur-manske@proton.me>
2024-10-24 19:46:43 +0000

    Initial commit
    Added project basic structure

---
+++ app/src/main/java/org/app/App.java
@@ -0,0 +1,11 @@
+package org.app;
+
+public class App
+{
+	public static String getGreeting() { return "Hello, world from Java!"; }
+
+	public static void main(String[] args)
+	{
+		System.out.println(App.getGreeting());
+	}
+};
